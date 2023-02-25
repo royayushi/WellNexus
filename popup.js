14942076a83fb51e4a8d32c4df756ea31cde64ba
royayushi <ayushiroy550@gmail.com>
2023-02-25 15:41:34 +0530

    Modified popup.json file by adding more time limits and by modifying the createAlarm function.

--- popup.js
+++ popup.js
@@ -1,7 +1,27 @@
 
-const ele = document.getElementById("btn")
-ele.addEventListener("click", () => {
-    chrome.runtime.sendMessage({ time: "0.5" }, function (response) {
-        console.log(response);
-    });
-});
+// ele.addEventListener("click", () => {
+//     chrome.runtime.sendMessage({ time: "0.5" }, function (response) {
+//         console.log(response);
+//     });
+// });
+
+function createAlarm(event) {
+    let minutes = parseFloat(event.target.value);
+    chrome.alarms.create({delayInMinutes: minutes});
+    chrome.storage.sync.set({minutes: minutes});
+    window.close();
+  }
+  
+//   function clearAlarm() {
+//     chrome.action.setBadgeText({text: ''});
+//     chrome.alarms.clearAll();
+//     window.close();
+//   }
+
+  document.getElementById('sampleMinute').addEventListener('click', createAlarm);
+  document.getElementById('min30').addEventListener('click', createAlarm);
+  document.getElementById('min60').addEventListener('click', createAlarm);
+  document.getElementById('min120').addEventListener('click', createAlarm);
+  document.getElementById('min180').addEventListener('click',createAlarm);
+  document.getElementById('min240').addEventListener('click', createAlarm);
+  document.getElementById('cancelAlarm').addEventListener('click', clearAlarm);
