bcbb02e7fe9623db73631845a11129b8d4149b11
Satyaki <satyakidawn@gmail.com>
2023-02-26 02:04:48 +0530

    Added Whole thing

---
+++ js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following foods is a good source of vitamin C?",
+    answer: "OrangesOranges",
+    options: [
+      "Chicken",
+      "Oranges",
+      "Cheese",
+      "White bread"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the recommended amount of physical activity for adults per week?",
+    answer: "60 minutes",
+    options: [
+      "30 minutes",
+      "60 minutes",
+      "90 minutes",
+      "120 minutes"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following diseases is caused by a virus?",
+    answer: "Influenza",
+    options: [
+      "Diabetes",
+      "Cancer",
+      "Tuberculosis",
+      "Influenzar"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is a symptom of depression?",
+    answer: "Insomnia",
+    options: [
+      "Insomnia",
+      "Hyperactivity",
+      "Increased appetite",
+      "Excitability"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is a risk factor for heart disease?",
+    answer: "Smoking",
+    options: [
+      "Eating a diet high in fruits and vegetables",
+      "Exercising regularly",
+      "Smoking",
+      "Getting enough sleep"
+    ]
+  },
+  
+    {
+    numb: 6,
+    question: "What is the normal range for blood pressure in adults?",
+    answer: "120/80 mmHg",
+    options: [
+      "100/60 mmHg",
+      "120/80 mmHg",
+      "140/90 mmHg",
+      "160/100 mmHg"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following nutrients is essential for strong bones?",
+    answer: "Vitamin D",
+    options: [
+      "Vitamin D",
+      "Vitamin C",
+      "Iron",
+      "Vitamin B12"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which of the following is a good way to reduce stress?",
+    answer: "Listening to music",
+    options: [
+      "Drinking alcohol",
+      "Eating junk food",
+      "Listening to music",
+      "Watching TV"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of the following is a recommended way to prevent the spread of germs?",
+    answer: "Covering your mouth with your hands when coughing or sneezing",
+    options: [
+      "Covering your mouth with your hands when coughing or sneezing",
+      "Not washing your hands",
+      "Touching your face frequently",
+      "Sharing personal items like towels and toothbrushes"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which of the following is an example of a complex carbohydrate?",
+    answer: "Brown rice",
+    options: [
+      "White bread",
+      "Sugar",
+      "Brown rice",
+      "Candy bar"
+    ]
+  },
+];
